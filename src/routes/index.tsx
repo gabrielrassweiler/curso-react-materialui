a9fa0ca5af106f70ca17749822e984810975bc70
Gabriel <gabriel@conectra.com.br>
2022-02-18 14:58:47 -0300

    [ADD] #material-ui - instalação das dependencias

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@mui/material";
 import { BrowserRouter, Routes as RoutesApp, Route, Navigate } from "react-router-dom";
 
 export const Routes = () => {
   return (
     <BrowserRouter>
       <RoutesApp>
-        <Route path="/" element={<p>Hello</p>}/>
+        <Route path="/" element={<Button>Button</Button>}/>
         <Route path="*" element={<Navigate to="/"/>}/>
       </RoutesApp>
     </BrowserRouter>
